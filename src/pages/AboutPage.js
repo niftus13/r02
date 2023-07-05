ac2dd49ff6f6cf68dcf34d56b8c49d1187ca804f
niftus13 <niftus13@gmail.com>
2023-07-05 16:59:03 +0900

    react 0705

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,16 @@
+import BasicLayout from "../layouts/BasicLayout";
+
+
+
+
+const AboutPage = () => {
+    return ( 
+
+        <BasicLayout>
+            <h2>About Page</h2>
+        </BasicLayout>
+
+     );
+}
+ 
+export default AboutPage;
